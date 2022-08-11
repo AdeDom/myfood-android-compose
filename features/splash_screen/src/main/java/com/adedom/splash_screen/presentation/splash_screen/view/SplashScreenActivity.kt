751a927b557860d97bf4f4d713a47e6b6511bfc8
pathiphon <pathiphon.ja@chococrm.com>
2022-08-12 00:22:40 +0700

    Create ui compose on feature splash screen.

--- features/splash_screen/src/main/java/com/adedom/splash_screen/presentation/splash_screen/view/SplashScreenActivity.kt
+++ features/splash_screen/src/main/java/com/adedom/splash_screen/presentation/splash_screen/view/SplashScreenActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.adedom.splash_screen.R
 import com.adedom.splash_screen.presentation.splash_screen.event.SplashScreenUiEvent
 import com.adedom.splash_screen.presentation.splash_screen.view_model.SplashScreenViewModel
 import com.adedom.splash_screen.presentation.ui.theme.MyFoodTheme
@@ -26,31 +33,53 @@ class SplashScreenActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val viewModel: SplashScreenViewModel by rememberInstance()
+
+                    val context = LocalContext.current
+
+                    LaunchedEffect(key1 = true) {
+                        viewModel.uiEvent.collect { uiEvent ->
+                            when (uiEvent) {
+                                SplashScreenUiEvent.Authentication -> {
+                                    Toast.makeText(context, "Authentication", Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+                                SplashScreenUiEvent.UnAuthentication -> {
+                                    Toast.makeText(context, "UnAuthentication", Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+                            }
+                        }
+                    }
+
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.bg),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize(),
+                        )
+
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.align(Alignment.Center),
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.logo_black),
+                                contentDescription = null,
+                                modifier = Modifier.width(200.dp),
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.logo_food_delivery),
+                                contentDescription = null,
+                                modifier = Modifier.width(120.dp),
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    val viewModel: SplashScreenViewModel by rememberInstance()
-
-    val context = LocalContext.current
-
-    LaunchedEffect(key1 = true) {
-        viewModel.uiEvent.collect { uiEvent ->
-            when (uiEvent) {
-                SplashScreenUiEvent.Authentication -> {
-                    Toast.makeText(context, "Authentication", Toast.LENGTH_SHORT).show()
-                }
-                SplashScreenUiEvent.UnAuthentication -> {
-                    Toast.makeText(context, "UnAuthentication", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    Text(text = "Hello $name!")
 }
