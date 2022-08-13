81d479f05af4ac57c538f6d4631d6dc0539301f3
pathiphon <pathiphon.ja@chococrm.com>
2022-08-13 16:32:25 +0700

    Clean code base and constant.

--- app/src/main/java/com/adedom/myfood/utils/constant/AppConstant.kt
+++
@@ -1,8 +0,0 @@
-package com.adedom.myfood.utils.constant
-
-object AppConstant {
-
-    const val PAGE = "page"
-    const val LOGIN_PAGE = "login_page"
-    const val REGISTER_PAGE = "register_page"
-}
