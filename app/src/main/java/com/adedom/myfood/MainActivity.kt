9d24f0a71980c777bf8003a449b5c29cb8e8d77b
pathiphon <pathiphon.ja@chococrm.com>
2022-08-14 23:13:30 +0700

    Remove surface on main.

--- app/src/main/java/com/adedom/myfood/MainActivity.kt
+++ app/src/main/java/com/adedom/myfood/MainActivity.kt
@@ -3,10 +3,6 @@ package com.adedom.myfood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -30,111 +26,106 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyFoodTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                ConnectivityCompose()
+
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.SplashScreen.route,
                 ) {
-                    ConnectivityCompose()
-
-                    val navController = rememberNavController()
-
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.SplashScreen.route,
-                    ) {
-                        composable(Screen.SplashScreen.route) {
-                            SplashScreen { uiEvent ->
-                                when (uiEvent) {
-                                    SplashScreenUiEvent.Authentication -> {
-                                        navController.navigate(Screen.Main.route) {
-                                            popUpTo(Screen.SplashScreen.route) {
-                                                inclusive = true
-                                            }
+                    composable(Screen.SplashScreen.route) {
+                        SplashScreen { uiEvent ->
+                            when (uiEvent) {
+                                SplashScreenUiEvent.Authentication -> {
+                                    navController.navigate(Screen.Main.route) {
+                                        popUpTo(Screen.SplashScreen.route) {
+                                            inclusive = true
                                         }
                                     }
-                                    SplashScreenUiEvent.UnAuthentication -> {
-                                        navController.navigate(Screen.Welcome.route) {
-                                            popUpTo(Screen.SplashScreen.route) {
-                                                inclusive = true
-                                            }
+                                }
+                                SplashScreenUiEvent.UnAuthentication -> {
+                                    navController.navigate(Screen.Welcome.route) {
+                                        popUpTo(Screen.SplashScreen.route) {
+                                            inclusive = true
                                         }
                                     }
                                 }
                             }
                         }
-                        composable(Screen.Welcome.route) {
-                            WelcomeScreen { uiEvent ->
-                                when (uiEvent) {
-                                    WelcomeUiEvent.Login -> {
-                                        navController.navigate(Screen.Login.route)
-                                    }
-                                    WelcomeUiEvent.Register -> {
-                                        navController.navigate(Screen.Register.route)
-                                    }
-                                    is WelcomeUiEvent.Skip -> {
-                                        navController.navigate(Screen.Main.route) {
-                                            popUpTo(Screen.Welcome.route) {
-                                                inclusive = true
-                                            }
+                    }
+                    composable(Screen.Welcome.route) {
+                        WelcomeScreen { uiEvent ->
+                            when (uiEvent) {
+                                WelcomeUiEvent.Login -> {
+                                    navController.navigate(Screen.Login.route)
+                                }
+                                WelcomeUiEvent.Register -> {
+                                    navController.navigate(Screen.Register.route)
+                                }
+                                is WelcomeUiEvent.Skip -> {
+                                    navController.navigate(Screen.Main.route) {
+                                        popUpTo(Screen.Welcome.route) {
+                                            inclusive = true
                                         }
                                     }
                                 }
                             }
                         }
-                        composable(Screen.Login.route) {
-                            LoginScreen { uiEvent ->
-                                when (uiEvent) {
-                                    LoginUiEvent.LoginSuccess -> {
-                                        navController.navigate(Screen.Main.route) {
-                                            popUpTo(Screen.Welcome.route) {
-                                                inclusive = true
-                                            }
+                    }
+                    composable(Screen.Login.route) {
+                        LoginScreen { uiEvent ->
+                            when (uiEvent) {
+                                LoginUiEvent.LoginSuccess -> {
+                                    navController.navigate(Screen.Main.route) {
+                                        popUpTo(Screen.Welcome.route) {
+                                            inclusive = true
                                         }
                                     }
-                                    LoginUiEvent.Register -> {
-                                        navController.popBackStack()
-                                        navController.navigate(Screen.Register.route)
-                                    }
+                                }
+                                LoginUiEvent.Register -> {
+                                    navController.popBackStack()
+                                    navController.navigate(Screen.Register.route)
                                 }
                             }
                         }
-                        composable(Screen.Register.route) {
-                            RegisterScreen { uiEvent ->
-                                when (uiEvent) {
-                                    RegisterUiEvent.Login -> {
-                                        navController.popBackStack()
-                                        navController.navigate(Screen.Login.route)
-                                    }
-                                    RegisterUiEvent.Register -> {}
+                    }
+                    composable(Screen.Register.route) {
+                        RegisterScreen { uiEvent ->
+                            when (uiEvent) {
+                                RegisterUiEvent.Login -> {
+                                    navController.popBackStack()
+                                    navController.navigate(Screen.Login.route)
                                 }
+                                RegisterUiEvent.Register -> {}
                             }
                         }
-                        composable(Screen.Main.route) {
-                            MainScreen { uiEvent ->
-                                when (uiEvent) {
-                                    MainUiEvent.Logout -> {
-                                        navController.navigate(Screen.Welcome.route) {
-                                            popUpTo(Screen.Main.route) {
-                                                inclusive = true
-                                            }
+                    }
+                    composable(Screen.Main.route) {
+                        MainScreen { uiEvent ->
+                            when (uiEvent) {
+                                MainUiEvent.Logout -> {
+                                    navController.navigate(Screen.Welcome.route) {
+                                        popUpTo(Screen.Main.route) {
+                                            inclusive = true
                                         }
                                     }
-                                    is MainUiEvent.FoodDetail -> {
-                                        navController.navigate(Screen.FoodDetail.arguments(uiEvent.foodId))
-                                    }
+                                }
+                                is MainUiEvent.FoodDetail -> {
+                                    navController.navigate(Screen.FoodDetail.arguments(uiEvent.foodId))
                                 }
                             }
                         }
-                        composable(
-                            route = Screen.FoodDetail.route,
-                            arguments = Screen.FoodDetail.arguments,
-                        ) { backStackEntry ->
-                            val foodId = backStackEntry.arguments?.getInt("foodId")
-                            FoodDetailScreen(foodId) { uiEvent ->
-                                when (uiEvent) {
-                                    FoodDetailUiEvent.OnBackPressed -> {
-                                        navController.popBackStack()
-                                    }
+                    }
+                    composable(
+                        route = Screen.FoodDetail.route,
+                        arguments = Screen.FoodDetail.arguments,
+                    ) { backStackEntry ->
+                        val foodId = backStackEntry.arguments?.getInt("foodId")
+                        FoodDetailScreen(foodId) { uiEvent ->
+                            when (uiEvent) {
+                                FoodDetailUiEvent.OnBackPressed -> {
+                                    navController.popBackStack()
                                 }
                             }
                         }
