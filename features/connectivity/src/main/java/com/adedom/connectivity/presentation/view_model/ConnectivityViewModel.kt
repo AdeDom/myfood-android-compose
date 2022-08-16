2567dca7aad2a991b012e87e30cff6a0c405cc0f
pathiphon <pathiphon.ja@chococrm.com>
2022-08-16 22:28:31 +0700

    Handle hide online network popup.

--- features/connectivity/src/main/java/com/adedom/connectivity/presentation/view_model/ConnectivityViewModel.kt
+++ features/connectivity/src/main/java/com/adedom/connectivity/presentation/view_model/ConnectivityViewModel.kt
@@ -1,21 +1,31 @@
 package com.adedom.connectivity.presentation.view_model
 
+import androidx.lifecycle.viewModelScope
 import com.adedom.connectivity.data.models.Status
 import com.adedom.connectivity.domain.use_cases.GetConnectivityStatusUseCase
 import com.adedom.connectivity.presentation.event.ConnectivityUiEvent
 import com.adedom.connectivity.presentation.state.ConnectivityUiState
 import com.adedom.ui_components.base.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class ConnectivityViewModel(
-    private val getConnectivityStatusUseCase: GetConnectivityStatusUseCase,
+    getConnectivityStatusUseCase: GetConnectivityStatusUseCase,
 ) : BaseViewModel<ConnectivityUiState, ConnectivityUiEvent>(ConnectivityUiState()) {
 
     init {
-        launch {
-            getConnectivityStatusUseCase().collect { status ->
+        getConnectivityStatusUseCase()
+            .onEach { status ->
                 uiState = uiState.copy(status = status)
             }
-        }
+            .filter { it == Status.Available }
+            .onEach {
+                delay(3000)
+                onDismissRequest()
+            }
+            .launchIn(viewModelScope)
     }
 
     fun onDismissRequest() {
