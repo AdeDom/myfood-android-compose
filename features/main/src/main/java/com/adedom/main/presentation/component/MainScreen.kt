5bfcc192999e70f47ffd68f0782cf1e38954ea0c
pathiphon <pathiphon.ja@chococrm.com>
2022-08-19 23:48:22 +0700

    Build ui component food list.

--- features/main/src/main/java/com/adedom/main/presentation/component/MainScreen.kt
+++ features/main/src/main/java/com/adedom/main/presentation/component/MainScreen.kt
@@ -12,10 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.adedom.main.R
 import com.adedom.main.presentation.event.MainUiEvent
@@ -85,80 +88,133 @@ fun MainContent(viewModel: MainViewModel) {
             .fillMaxSize()
             .padding(16.dp),
     ) {
-        LazyColumn {
-            item {
-                Column {
-                    Row {
-                        AppTitleText(text = "Food")
-                        Spacer(modifier = Modifier.weight(1f))
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_logout_gray),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(
-                                    width = 24.dp,
-                                    height = 24.dp,
-                                ),
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-                    AppTextField(
-                        value = viewModel.uiState.search,
-                        onValueChange = viewModel::setSearch,
-                        hint = "Search food",
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_search_black),
-                                contentDescription = null,
-                            )
-                        },
-                        modifier = Modifier.fillMaxWidth(),
+        Column {
+            Column {
+                Row {
+                    AppTitleText(text = "Food")
+                    Spacer(modifier = Modifier.weight(1f))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_logout_gray),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(
+                                width = 24.dp,
+                                height = 24.dp,
+                            ),
                     )
                 }
+                Spacer(modifier = Modifier.height(16.dp))
+                AppTextField(
+                    value = viewModel.uiState.search,
+                    onValueChange = viewModel::setSearch,
+                    hint = "Search food",
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search_black),
+                            contentDescription = null,
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
             }
 
-            item {
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyRow {
-                    items(viewModel.uiState.categoryList) { category ->
-                        Box(
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .clickable {
-                                    viewModel.getFoodListByCategoryId(category.categoryId)
-                                },
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyRow {
+                items(viewModel.uiState.categoryList) { category ->
+                    Box(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .clickable {
+                                viewModel.getFoodListByCategoryId(category.categoryId)
+                            },
+                    ) {
+                        Card(
+                            shape = RoundedCornerShape(8.dp),
+                            elevation = 8.dp,
                         ) {
-                            Card(
-                                shape = RoundedCornerShape(8.dp),
-                                elevation = 8.dp,
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
                             ) {
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                ) {
-                                    AsyncImage(
-                                        model = category.image,
-                                        contentDescription = null,
-                                        contentScale = ContentScale.Crop,
-                                        modifier = Modifier.size(
-                                            width = 100.dp,
-                                            height = 100.dp,
-                                        )
+                                AsyncImage(
+                                    model = category.image,
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier.size(
+                                        width = 100.dp,
+                                        height = 100.dp,
                                     )
-                                    Spacer(modifier = Modifier.height(4.dp))
-                                    AppText(
-                                        text = category.categoryName,
-                                        fontWeight = FontWeight.Bold,
-                                    )
-                                }
+                                )
+                                Spacer(modifier = Modifier.height(4.dp))
+                                AppText(
+                                    text = category.categoryName,
+                                    fontWeight = FontWeight.Bold,
+                                )
                             }
                         }
                     }
                 }
             }
 
-            item {
-                Spacer(modifier = Modifier.height(16.dp))
-                AppTitleText(text = viewModel.uiState.categoryName)
+            Spacer(modifier = Modifier.height(16.dp))
+            AppTitleText(text = viewModel.uiState.categoryName)
+
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn {
+                items(viewModel.uiState.foodList) { food ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .clickable {
+                                viewModel.onFoodDetailEvent(food.foodId)
+                            },
+                    ) {
+                        Row {
+                            AsyncImage(
+                                model = food.image,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(
+                                        width = 100.dp,
+                                        height = 100.dp,
+                                    )
+                                    .clip(RoundedCornerShape(8.dp)),
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Column {
+                                AppText(
+                                    text = food.foodName,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                                food.alias?.let {
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    AppText(
+                                        text = food.alias,
+                                        color = Color.Gray,
+                                        fontSize = 14.sp,
+                                    )
+                                }
+                                food.ratingScoreCount?.let {
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Row {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.ic_star_amber),
+                                            contentDescription = null,
+                                            modifier = Modifier.size(18.dp),
+                                        )
+                                        Spacer(modifier = Modifier.width(4.dp))
+                                        AppText(
+                                            text = food.ratingScoreCount,
+                                            color = Color(0xFFFFC107),
+                                            fontSize = 14.sp,
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
