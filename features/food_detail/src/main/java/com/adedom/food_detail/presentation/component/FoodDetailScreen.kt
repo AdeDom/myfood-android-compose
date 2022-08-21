23045fd88fb068a449f99f0e6f6b37d53cccf652
pathiphon <pathiphon.ja@chococrm.com>
2022-08-21 15:40:07 +0700

    Refactor preview food detail.

--- features/food_detail/src/main/java/com/adedom/food_detail/presentation/component/FoodDetailScreen.kt
+++ features/food_detail/src/main/java/com/adedom/food_detail/presentation/component/FoodDetailScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.unit.sp
 import com.adedom.food_detail.R
 import com.adedom.food_detail.domain.models.FoodDetailModel
 import com.adedom.food_detail.presentation.event.FoodDetailUiEvent
+import com.adedom.food_detail.presentation.state.FoodDetailUiState
 import com.adedom.food_detail.presentation.view_model.FoodDetailViewModel
 import com.adedom.ui_components.components.AppErrorAlertDialog
 import com.adedom.ui_components.components.AppImage
@@ -45,6 +46,21 @@ fun FoodDetailScreen(
 
     val state = viewModel.uiState
 
+    FoodDetailContent(
+        state = state,
+        onErrorDismiss = viewModel::setOnBackPressedEvent,
+    )
+}
+
+@Composable
+fun FoodDetailContent(
+    state: FoodDetailUiState,
+    onErrorDismiss: () -> Unit,
+) {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.dp
+    val screenHeightDp = configuration.screenHeightDp.dp
+
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -57,112 +73,101 @@ fun FoodDetailScreen(
         if (state.error != null) {
             AppErrorAlertDialog(
                 error = state.error,
-                onDismiss = viewModel::setOnBackPressedEvent,
+                onDismiss = onErrorDismiss,
             )
         }
 
         state.foodDetail?.let { foodDetail ->
-            FoodDetailContent(foodDetail)
-        }
-    }
-}
-
-@Composable
-fun FoodDetailContent(
-    foodDetail: FoodDetailModel,
-) {
-    val configuration = LocalConfiguration.current
-    val screenWidthDp = configuration.screenWidthDp.dp
-    val screenHeightDp = configuration.screenHeightDp.dp
-
-    AppImageNetwork(
-        image = foodDetail.image,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(screenHeightDp / 2),
-    )
-    LazyColumn {
-        item {
-            Spacer(
-                modifier = Modifier.height((screenHeightDp / 2) - 128.dp),
+            AppImageNetwork(
+                image = foodDetail.image,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(screenHeightDp / 2),
             )
-            Box {
-                Column {
+            LazyColumn {
+                item {
                     Spacer(
-                        modifier = Modifier.height(64.dp),
+                        modifier = Modifier.height((screenHeightDp / 2) - 128.dp),
                     )
-                    Card(
-                        shape = RoundedCornerShape(
-                            topStart = 64.dp,
-                            topEnd = 64.dp,
-                        ),
-                        elevation = 16.dp,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(screenHeightDp - 64.dp),
-                    ) {
-                        Column(
-                            modifier = Modifier.padding(
-                                start = 32.dp,
-                                end = 32.dp,
-                                top = 32.dp,
+                    Box {
+                        Column {
+                            Spacer(
+                                modifier = Modifier.height(64.dp),
                             )
-                        ) {
-                            Text(
-                                text = foodDetail.foodName,
-                                color = Color.Black,
-                                fontSize = 24.sp,
-                            )
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Row(
-                                verticalAlignment = Alignment.Bottom,
+                            Card(
+                                shape = RoundedCornerShape(
+                                    topStart = 64.dp,
+                                    topEnd = 64.dp,
+                                ),
+                                elevation = 16.dp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(screenHeightDp - 64.dp),
                             ) {
-                                AppImage(
-                                    image = R.drawable.ic_star_amber,
-                                    modifier = Modifier.size(18.dp),
-                                )
-                                Spacer(modifier = Modifier.width(4.dp))
-                                Text(
-                                    text = foodDetail.ratingScoreCount,
-                                    color = Color(0xFFFFC107),
-                                    fontSize = 14.sp,
-                                )
-                                Box(modifier = Modifier.weight(1f))
-                                Text(
-                                    text = "${foodDetail.price}",
-                                    fontSize = 32.sp,
-                                    fontWeight = FontWeight.Bold,
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = "Bath",
-                                    fontSize = 32.sp,
-                                    fontWeight = FontWeight.Bold,
-                                )
+                                Column(
+                                    modifier = Modifier.padding(
+                                        start = 32.dp,
+                                        end = 32.dp,
+                                        top = 32.dp,
+                                    )
+                                ) {
+                                    Text(
+                                        text = foodDetail.foodName,
+                                        color = Color.Black,
+                                        fontSize = 24.sp,
+                                    )
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    Row(
+                                        verticalAlignment = Alignment.Bottom,
+                                    ) {
+                                        AppImage(
+                                            image = R.drawable.ic_star_amber,
+                                            modifier = Modifier.size(18.dp),
+                                        )
+                                        Spacer(modifier = Modifier.width(4.dp))
+                                        Text(
+                                            text = foodDetail.ratingScoreCount,
+                                            color = Color(0xFFFFC107),
+                                            fontSize = 14.sp,
+                                        )
+                                        Box(modifier = Modifier.weight(1f))
+                                        Text(
+                                            text = "${foodDetail.price}",
+                                            fontSize = 32.sp,
+                                            fontWeight = FontWeight.Bold,
+                                        )
+                                        Spacer(modifier = Modifier.width(8.dp))
+                                        Text(
+                                            text = "Bath",
+                                            fontSize = 32.sp,
+                                            fontWeight = FontWeight.Bold,
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.height(32.dp))
+                                    Text(
+                                        text = "Description",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp,
+                                    )
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    Text(
+                                        text = foodDetail.description,
+                                        fontSize = 16.sp,
+                                    )
+                                }
                             }
-                            Spacer(modifier = Modifier.height(32.dp))
-                            Text(
-                                text = "Description",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 16.sp,
-                            )
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Text(
-                                text = foodDetail.description,
-                                fontSize = 16.sp,
-                            )
                         }
+                        AppImage(
+                            image = R.drawable.favorite_active,
+                            modifier = Modifier
+                                .size(80.dp)
+                                .offset(
+                                    x = screenWidthDp - 128.dp,
+                                    y = 16.dp,
+                                ),
+                        )
                     }
                 }
-                AppImage(
-                    image = R.drawable.favorite_active,
-                    modifier = Modifier
-                        .size(80.dp)
-                        .offset(
-                            x = screenWidthDp - 128.dp,
-                            y = 16.dp,
-                        ),
-                )
             }
         }
     }
@@ -172,13 +177,17 @@ fun FoodDetailContent(
 @Composable
 fun FoodDetailContentPreview() {
     MyFoodTheme {
-        val foodDetail = FoodDetailModel(
-            foodName = "",
-            image = "",
-            price = 0.0,
-            description = "",
-            ratingScoreCount = "",
+        FoodDetailContent(
+            state = FoodDetailUiState(
+                foodDetail = FoodDetailModel(
+                    foodName = "foodName",
+                    image = "",
+                    price = 99.0,
+                    description = "description",
+                    ratingScoreCount = "4.9",
+                )
+            ),
+            onErrorDismiss = {},
         )
-        FoodDetailContent(foodDetail)
     }
 }
