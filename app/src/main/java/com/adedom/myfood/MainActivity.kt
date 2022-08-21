5a31f402a74dcf4b60c321571852ee3901ef2c0b
pathiphon <pathiphon.ja@chococrm.com>
2022-08-21 16:30:08 +0700

    Preview connectivity content.

--- app/src/main/java/com/adedom/myfood/MainActivity.kt
+++ app/src/main/java/com/adedom/myfood/MainActivity.kt
@@ -3,7 +3,7 @@ package com.adedom.myfood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.adedom.connectivity.presentation.component.ConnectivityCompose
+import com.adedom.connectivity.presentation.component.ConnectivityScreen
 import com.adedom.myfood.presentation.component.MainAppNavHost
 import com.adedom.ui_components.theme.MyFoodTheme
 
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyFoodTheme {
-                ConnectivityCompose()
+                ConnectivityScreen()
                 MainAppNavHost()
             }
         }
--- features/connectivity/src/main/java/com/adedom/connectivity/presentation/component/ConnectivityCompose.kt
+++ features/connectivity/src/main/java/com/adedom/connectivity/presentation/component/ConnectivityCompose.kt
@@ -1,5 +1,6 @@
 package com.adedom.connectivity.presentation.component
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,19 +9,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import com.adedom.connectivity.data.models.Status
+import com.adedom.connectivity.presentation.state.ConnectivityUiState
 import com.adedom.connectivity.presentation.view_model.ConnectivityViewModel
+import com.adedom.ui_components.theme.MyFoodTheme
 import org.kodein.di.compose.rememberInstance
 
 @Composable
-fun ConnectivityCompose() {
+fun ConnectivityScreen() {
     val viewModel by rememberInstance<ConnectivityViewModel>()
 
-    when (viewModel.uiState.status) {
+    ConnectivityContent(
+        state = viewModel.uiState,
+        onDismissRequest = viewModel::onDismissRequest,
+    )
+}
+
+@Composable
+fun ConnectivityContent(
+    state: ConnectivityUiState,
+    onDismissRequest: () -> Unit,
+) {
+    when (state.status) {
         Status.Available -> {
-            OnlineNetworkPopup(viewModel::onDismissRequest)
+            OnlineNetworkPopup(onDismissRequest)
         }
         Status.Unavailable -> {
             OfflineNetworkPopup()
@@ -72,5 +88,37 @@ private fun OfflineNetworkPopup() {
                 modifier = Modifier.align(Alignment.Center),
             )
         }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ConnectivityContentAvailablePreview() {
+    val context = LocalContext.current
+    MyFoodTheme {
+        ConnectivityContent(
+            state = ConnectivityUiState(
+                status = Status.Available,
+            ),
+            onDismissRequest = {
+                Toast.makeText(context, "onDismissRequest", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ConnectivityContentUnavailablePreview() {
+    val context = LocalContext.current
+    MyFoodTheme {
+        ConnectivityContent(
+            state = ConnectivityUiState(
+                status = Status.Unavailable,
+            ),
+            onDismissRequest = {
+                Toast.makeText(context, "onDismissRequest", Toast.LENGTH_SHORT).show()
+            }
+        )
     }
 }
