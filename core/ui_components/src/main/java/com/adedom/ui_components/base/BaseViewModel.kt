e32757557911b5d6a803478b85d64bc1709bd77c
pathiphon <pathiphon.ja@chococrm.com>
2022-09-06 00:02:04 +0700

    Update base view model for mvi or bloc.

--- core/ui_components/src/main/java/com/adedom/ui_components/base/BaseViewModel.kt
+++ core/ui_components/src/main/java/com/adedom/ui_components/base/BaseViewModel.kt
@@ -5,18 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlin.coroutines.CoroutineContext
 
-/**
- *@param S :: State - render ui
- *@param E :: Event - nav or single live event
- *@param A :: Action - ui action
- */
-abstract class BaseViewModel<S : Any, E : Any, A : Any>(
-    initialUiState: S,
+abstract class BaseViewModel<Event : Any, State : Any>(
+    initialUiState: State,
 ) : ViewModel(), CoroutineScope {
 
     private val job = SupervisorJob()
@@ -27,27 +19,18 @@ abstract class BaseViewModel<S : Any, E : Any, A : Any>(
     var uiState by mutableStateOf(initialUiState)
         private set
 
-    private val _uiEvent = MutableSharedFlow<E>()
-    val uiEvent: SharedFlow<E> = _uiEvent.asSharedFlow()
-
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.Main + exceptionHandler
 
-    protected fun setState(reducer: S.() -> S) {
+    protected fun setState(reducer: State.() -> State) {
         uiState = uiState.reducer()
     }
 
-    protected suspend fun coState(reducer: suspend S.() -> S) {
+    protected suspend fun coState(reducer: suspend State.() -> State) {
         uiState = uiState.reducer()
     }
 
-    protected fun setEvent(event: E) {
-        launch {
-            _uiEvent.emit(event)
-        }
-    }
-
-    abstract fun dispatch(action: A)
+    abstract fun dispatch(action: Event)
 
     override fun onCleared() {
         coroutineContext.cancel()
