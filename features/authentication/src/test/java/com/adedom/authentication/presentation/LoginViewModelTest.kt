064d0710b7d33edfa17a41395d41c909d5a8e23d
pathiphon <pathiphon.ja@chococrm.com>
2022-09-05 23:12:42 +0700

    Update test login view model.

--- features/authentication/src/test/java/com/adedom/authentication/presentation/LoginViewModelTest.kt
+++ features/authentication/src/test/java/com/adedom/authentication/presentation/LoginViewModelTest.kt
@@ -5,7 +5,6 @@ import com.adedom.authentication.domain.use_cases.LoginUseCase
 import com.adedom.authentication.domain.use_cases.ValidateEmailUseCase
 import com.adedom.authentication.domain.use_cases.ValidatePasswordUseCase
 import com.adedom.authentication.presentation.view_model.LoginUiAction
-import com.adedom.authentication.presentation.view_model.LoginUiEvent
 import com.adedom.authentication.presentation.view_model.LoginViewModel
 import com.adedom.authentication.utils.MainCoroutineRule
 import com.adedom.core.utils.Resource
@@ -16,7 +15,6 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -129,14 +127,12 @@ class LoginViewModelTest {
         val resourceError = Resource.Success(Unit)
         coEvery { loginUseCase(any(), any()) } returns resourceError
 
-        launch {
-            viewModel.dispatch(LoginUiAction.SetEmail(email))
-            viewModel.dispatch(LoginUiAction.SetPassword(password))
-            viewModel.dispatch(LoginUiAction.Submit)
-        }
+        viewModel.dispatch(LoginUiAction.SetEmail(email))
+        viewModel.dispatch(LoginUiAction.SetPassword(password))
+        viewModel.dispatch(LoginUiAction.Submit)
 
-        val result = viewModel.uiEvent.firstOrNull()
-        assertThat(result).isEqualTo(LoginUiEvent.NavMain)
+        val result = viewModel.nav.firstOrNull()
+        assertThat(result).isEqualTo(Unit)
         coVerify { loginUseCase(any(), any()) }
     }
 
@@ -146,15 +142,5 @@ class LoginViewModelTest {
 
         val result = viewModel.uiState
         assertThat(result.error).isNull()
-    }
-
-    @Test
-    fun `on click register event`() = runTest {
-        launch {
-            viewModel.dispatch(LoginUiAction.NavRegister)
-        }
-
-        val result = viewModel.uiEvent.firstOrNull()
-        assertThat(result).isEqualTo(LoginUiEvent.NavRegister)
     }
 }
