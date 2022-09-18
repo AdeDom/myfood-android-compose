558368d6d61eae425c1b788928b09762d7d1b70e
pathiphon <pathiphon.ja@chococrm.com>
2022-09-18 14:56:53 +0700

    Refactor try catch on home content use case.

--- features/main/src/main/java/com/adedom/main/domain/use_cases/HomeContentUseCase.kt
+++ features/main/src/main/java/com/adedom/main/domain/use_cases/HomeContentUseCase.kt
@@ -1,7 +1,6 @@
 package com.adedom.main.domain.use_cases
 
 import com.adedom.core.data.Resource
-import com.adedom.core.utils.ApiServiceException
 import com.adedom.main.domain.models.CategoryModel
 import com.adedom.main.domain.repositories.HomeCategoryRepository
 import com.adedom.main.domain.repositories.HomeFoodRepository
@@ -18,59 +17,54 @@ class HomeContentUseCase(
 ) {
 
     suspend operator fun invoke(): Resource<List<CategoryModel>> {
-        return try {
-            coroutineScope {
-                // category
-                val categories = homeCategoryRepository.callCategoryAll()
-                val categoryEntity = categories.map { category ->
-                    CategoryEntity(
-                        categoryId = category.categoryId.toLong(),
-                        categoryName = category.categoryName,
-                        categoryTypeName = category.categoryTypeName,
-                        created = category.created,
-                        image = category.image,
-                        updated = category.updated,
-                    )
-                }
-                homeCategoryRepository.deleteCategoryAll()
-                homeCategoryRepository.saveCategoryAll(categoryEntity)
-
-                // food
-                val foods = categories
-                    .map { category ->
-                        async {
-                            homeFoodRepository.callFoodListByCategoryId(category.categoryId)
-                        }
-                    }
-                    .awaitAll()
-                    .flatten()
-                val foodEntity = foods.map { food ->
-                    FoodEntity(
-                        alias = food.alias,
-                        categoryId = food.categoryId.toLong(),
-                        created = food.created,
-                        description = food.description,
-                        favorite = food.favorite,
-                        foodId = food.foodId.toLong(),
-                        foodName = food.foodName,
-                        search = food.foodName,
-                        image = food.image,
-                        price = food.price,
-                        ratingScore = food.ratingScore?.toDouble(),
-                        ratingScoreCount = food.ratingScoreCount,
-                        status = food.status,
-                        updated = food.updated,
-                    )
-                }
-                homeFoodRepository.deleteFoodAll()
-                homeFoodRepository.saveFoodAll(foodEntity)
-
-                val categoriesModel = categories.map { mapCategoryToCategoryModel(it) }
-                Resource.Success(categoriesModel)
+        return coroutineScope {
+            // category
+            val categories = homeCategoryRepository.callCategoryAll()
+            val categoryEntity = categories.map { category ->
+                CategoryEntity(
+                    categoryId = category.categoryId.toLong(),
+                    categoryName = category.categoryName,
+                    categoryTypeName = category.categoryTypeName,
+                    created = category.created,
+                    image = category.image,
+                    updated = category.updated,
+                )
             }
-        } catch (exception: ApiServiceException) {
-            val baseError = exception.toBaseError()
-            Resource.Error(baseError)
+            homeCategoryRepository.deleteCategoryAll()
+            homeCategoryRepository.saveCategoryAll(categoryEntity)
+
+            // food
+            val foods = categories
+                .map { category ->
+                    async {
+                        homeFoodRepository.callFoodListByCategoryId(category.categoryId)
+                    }
+                }
+                .awaitAll()
+                .flatten()
+            val foodEntity = foods.map { food ->
+                FoodEntity(
+                    alias = food.alias,
+                    categoryId = food.categoryId.toLong(),
+                    created = food.created,
+                    description = food.description,
+                    favorite = food.favorite,
+                    foodId = food.foodId.toLong(),
+                    foodName = food.foodName,
+                    search = food.foodName,
+                    image = food.image,
+                    price = food.price,
+                    ratingScore = food.ratingScore?.toDouble(),
+                    ratingScoreCount = food.ratingScoreCount,
+                    status = food.status,
+                    updated = food.updated,
+                )
+            }
+            homeFoodRepository.deleteFoodAll()
+            homeFoodRepository.saveFoodAll(foodEntity)
+
+            val categoriesModel = categories.map { mapCategoryToCategoryModel(it) }
+            Resource.Success(categoriesModel)
         }
     }
 
