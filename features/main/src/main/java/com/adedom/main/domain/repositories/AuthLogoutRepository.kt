08b7cbb1b90741ddf4b1b0d591c02d014a3dd399
pathiphon <pathiphon.ja@chococrm.com>
2022-09-18 14:32:04 +0700

    Refactor resource to repository on logout use case.

--- features/main/src/main/java/com/adedom/main/domain/repositories/AuthLogoutRepository.kt
+++ features/main/src/main/java/com/adedom/main/domain/repositories/AuthLogoutRepository.kt
@@ -1,10 +1,11 @@
 package com.adedom.main.domain.repositories
 
+import com.adedom.core.data.Resource
 import com.adedom.core.utils.AuthRole
 
 interface AuthLogoutRepository {
 
-    suspend fun callLogout(): String?
+    suspend fun callLogout(): Resource<Unit>
 
     suspend fun getAuthRole(): AuthRole
 
