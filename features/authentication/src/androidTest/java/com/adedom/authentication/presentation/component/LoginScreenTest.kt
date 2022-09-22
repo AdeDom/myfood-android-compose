f18281a0241acfdb2669d3cb40a772dfbdb5d935
pathiphon <pathiphon.ja@chococrm.com>
2022-09-22 23:02:13 +0700

    Refactor build gradle group.

---
+++ features/authentication/src/androidTest/java/com/adedom/authentication/presentation/component/LoginScreenTest.kt
@@ -0,0 +1,10 @@
+package com.adedom.authentication.presentation.component
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class LoginScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
